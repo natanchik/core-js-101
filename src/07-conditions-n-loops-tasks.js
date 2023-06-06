5ad34b23fd3e239fa82643de4a3c2b53bd6077ea
Natalia Lebedeva <nataliadaveng@gmail.com>
2023-06-06 22:44:00 +0600

    feat: resolve loops task

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -443,10 +443,26 @@ function getCommonDirectoryPath(pathes) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const ans = Array(m1.length).fill(0);
+  for (let i = 0; i < m1.length; i += 1) {
+    ans[i] = Array(m2[0].length).fill(0);
+  }
+  for (let i = 0; i < m1.length; i += 1) {
+    for (let j = 0; j < m2[0].length; j += 1) {
+      let m = 0;
+      if (m2.length > 1) {
+        for (let k = 0; k < m2.length; k += 1) {
+          m += m1[i][k] * m2[k][j];
+        }
+      } else {
+        m = m1[i][j] * m2[j][i];
+      }
+      ans[i][j] = m;
+    }
+  }
+  return ans;
 }
-
 
 /**
  * Returns the evaluation of the specified tic-tac-toe position.
